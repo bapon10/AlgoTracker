8059dcdfb64b9151f0dd9c46acb013e85e9b68d2
bapon10 <baponr10032003@gmail.com>
2025-06-01 15:44:10 +0530

    Initial commit: base frontend + placeholder backend

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <>
+            <h1>AlgoTracker</h1>
+            <p>Track your DSA progress across LeetCode, Codeforces and more</p>
+            <button>Get Started</button>
+        </>
+
+    );
+}
